9fafb9e5f414de0e635a575abe878193389539c2
Paul Gadi <paulgadi@gmail.com>
2023-04-02 19:09:16 -0400

    Update ora.tsx

--- pages/example/ora.tsx
+++ pages/example/ora.tsx
@@ -1,14 +1,13 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { OraImage } from "components/oraimage";
 
 const OraPage: NextPage = () => {
   const router = useRouter();
 
 
   return (
-    <OraImage>
-    </OraImage>
+    <>
+    </>
   );
 };
 export default OraPage;
